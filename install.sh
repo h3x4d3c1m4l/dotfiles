be21f3d3690350ed91d5aae0d8d29c0c8274bb41
Sander in 't Hout <sander.ashwin@live.nl>
2018-10-22 22:10:47 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ dotfiles=~/Sources/dotfiles
 ln -sf $dotfiles/.Xmodmap ~/.Xmodmap
 ln -sf $dotfiles/.Xresources ~/.Xresources
 ln -sf $dotfiles/.xsession ~/.xsession
+mkdir -p $dotfiles/.config/i3 > /dev/null 2>&1
 ln -sf $dotfiles/.config/i3/config ~/.config/i3/config
