d134345ac442330586c44d39a9227d42b9869d8e
Sander in 't Hout <sander@inthout.eu>
2018-11-04 11:49:58 +0100

    fix for dotnet tooling, add basic i3blocks config

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ ln -sf $dotfiles/.Xmodmap ~/.Xmodmap
 ln -sf $dotfiles/.Xresources ~/.Xresources
 ln -sf $dotfiles/.xsession ~/.xsession
 mkdir -p ~/.config/i3 > /dev/null 2>&1
+mkdir -p ~/.config/i3blocks > /dev/null 2>&1
 ln -sf $dotfiles/.config/i3/config ~/.config/i3/config
+ln -sf $dotfiles/.config/i3blocks/config ~/.config/i3blocks/config
 ln -sf $dotfiles/.zshrc ~/.zshrc
