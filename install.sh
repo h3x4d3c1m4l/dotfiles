3ce558d2b8efd98b8c619c6c73cf9ce707b9064f
Sander in 't Hout <sander@inthout.eu>
2019-04-06 23:18:33 +0200

    add some more pre install stuff I commonly use, add guake settings restore

--- install.sh
+++ install.sh
@@ -2,26 +2,38 @@
 
 dotfiles=~/Sources/dotfiles
 
-# dependencies
-sudo pacman -S --needed xorg-xinput kate irqbalance rng-tools openssh
-sudo pacman -S --needed workrave
+# prechecks
+if ! [ -x "$(command -v pacaur)" ]; then
+  echo 'pacaur not installed!' >&2
+  exit 1
+fi
 
-# timezone, locale
+# dependencies
+pacaur -S --needed nerd-fonts-complete
+sudo pacman -S --needed git wget base-devel joe fish networkmanager irqbalance rng-tools openssh 
+sudo pacman -S --needed xorg-xinput guake kate workrave firefox chromium
+
+# timezone, locale, default shell
 sudo timedatectl set-timezone Europe/Amsterdam
 sudo localectl set-locale LANG=nl_NL.UTF-8 LC_MESSAGES=en_US.UTF-8
+chsh -s /usr/bin/fish
 
 # services
 sudo systemctl enable --now rngd
 sudo systemctl enable --now irqbalance
 sudo systemctl enable --now systemd-timesyncd
+sudo systemctl enable --now NetworkManager
 
 # user config files
+dconf reset -f /apps/guake/
+dconf load /apps/guake/ < $dotfiles/guake.settings
 ln -sf $dotfiles/.Xmodmap ~/.Xmodmap
 ln -sf $dotfiles/.Xresources ~/.Xresources
 ln -sf $dotfiles/.xsession ~/.xsession
 mkdir -p ~/.config/i3 > /dev/null 2>&1
 mkdir -p ~/.config/i3blocks > /dev/null 2>&1
 mkdir -p ~/.config/systemd/user > /dev/null 2>&1
+mkdir -p ~/.gconf/apps/guake > /dev/null 2>&1
 ln -sf $dotfiles/.config/i3/config ~/.config/i3/config
 ln -sf $dotfiles/.config/i3blocks/config ~/.config/i3blocks/config
 ln -sf $dotfiles/.zshrc ~/.zshrc
