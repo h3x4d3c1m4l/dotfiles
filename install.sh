c244d11b19f9cca489d619ef03f02f6be0ea648e
Sander in 't Hout <sander@inthout.eu>
2019-04-06 22:34:04 +0200

    add ssh-agent service

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 dotfiles=~/Sources/dotfiles
 
 # dependencies
-sudo pacman -S --needed xorg-xinput kate irqbalance rng-tools
+sudo pacman -S --needed xorg-xinput kate irqbalance rng-tools openssh
 sudo pacman -S --needed workrave
 
 # timezone, locale
@@ -21,9 +21,12 @@ ln -sf $dotfiles/.Xresources ~/.Xresources
 ln -sf $dotfiles/.xsession ~/.xsession
 mkdir -p ~/.config/i3 > /dev/null 2>&1
 mkdir -p ~/.config/i3blocks > /dev/null 2>&1
+mkdir -p ~/.config/systemd/user > /dev/null 2>&1
 ln -sf $dotfiles/.config/i3/config ~/.config/i3/config
 ln -sf $dotfiles/.config/i3blocks/config ~/.config/i3blocks/config
 ln -sf $dotfiles/.zshrc ~/.zshrc
+ln -sf $dotfiles/.config/systemd/user/ssh-agent.service ~/.config/systemd/user/ssh-agent.service
+systemctl --user enable --now ssh-agent.service
 
 # global config files
 sudo ln -sf $dotfiles/profile.d/* /etc/profile.d/
