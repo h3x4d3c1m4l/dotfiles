3736c50d855e40900ddb8c42381aaa8f83f67ce1
Sander in 't Hout <sander@inthout.eu>
2019-04-27 12:52:36 +0200

    add mako to startup apps

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ fi
 pacaur -S --needed nerd-fonts-hack
 sudo pacman -S --needed wget base-devel joe fish irqbalance rng-tools openssh thefuck
 sudo pacman -S --needed firefox chromium keepassxc gnome-keyring telegram-desktop nextcloud-client
-sudo pacman -S --needed sway waybar termite rofi ttf-font-awesome
+sudo pacman -S --needed sway waybar termite rofi ttf-font-awesome mako
 sudo pacman -S --needed p7zip unrar unarchiver lzop lrzip cpio arj lha lrzip lzip lzop unarj poppler-glib libgsf gvfs-mtp gvfs-gphoto2 gvfs-smb file-roller ark xarchiver gvfs tumbler thunar-volman thunar-archive-plugin thunar
 
 # timezone, locale, default shell
