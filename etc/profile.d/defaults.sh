996cc8b918b31cc30613194e2f2c42248434af9c
Sander in 't Hout <sander@inthout.eu>
2019-11-23 00:50:00 +0100

    remove unneeded hacks, add Firefox Wayland hack

--- etc/profile.d/defaults.sh
+++ etc/profile.d/defaults.sh
@@ -1,10 +1,6 @@
 export EDITOR="$(if [ "$XDG_SESSION_TYPE" != 'tty' ]; then echo 'kate'; else echo 'joe'; fi)"
 export RUSTC_WRAPPER=sccache
-#makes GNOME on Wayland crash :(
-#export GDK_BACKEND="$(if [ "$XDG_SESSION_TYPE" == 'wayland' ]; then echo 'wayland'; else echo 'x11'; fi)"
-export ELECTRON_TRASH=trash-cli
-alias code='GDK_BACKEND=x11 /usr/bin/code 2>/dev/null'
 
-# https://github.com/aspnet/DotNetTools/issues/399
-export DOTNET_ROOT=$(dirname $(realpath $(which dotnet)))
-export MSBuildSDKsPath=/opt/dotnet/sdk/$(dotnet --version)/Sdks
+### HACKS
+export MOZ_ENABLE_WAYLAND=1
+export ELECTRON_TRASH=trash-cli
