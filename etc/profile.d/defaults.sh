740fa42edf4d9b2364a91b2d2e570e5e6b51a957
Sander in 't Hout <sander@inthout.eu>
2019-11-22 01:24:01 +0100

    fix GNOME on Wayland crash

--- etc/profile.d/defaults.sh
+++ etc/profile.d/defaults.sh
@@ -1,6 +1,7 @@
 export EDITOR="$(if [ "$XDG_SESSION_TYPE" != 'tty' ]; then echo 'kate'; else echo 'joe'; fi)"
 export RUSTC_WRAPPER=sccache
-export GDK_BACKEND="$(if [ "$XDG_SESSION_TYPE" == 'wayland' ]; then echo 'wayland'; else echo 'x11'; fi)"
+#makes GNOME on Wayland crash :(
+#export GDK_BACKEND="$(if [ "$XDG_SESSION_TYPE" == 'wayland' ]; then echo 'wayland'; else echo 'x11'; fi)"
 export ELECTRON_TRASH=trash-cli
 alias code='GDK_BACKEND=x11 /usr/bin/code 2>/dev/null'
 
